8f9943d5ccca986e590e2f1628bb02564913ec1c
Rayane Santos <rayanesantos-131@hotmail.com>
2021-11-23 19:59:17 -0300

    adicao endpoints

--- index.js
+++ index.js
@@ -54,6 +54,93 @@ const playlists = [
     }
 ]
 
+const recentlyplayed = [
+    {
+        "title": "This is Charlie Brown Jr.",
+        "subtitle":
+            "Da baixada santista para os maiores palcos do Brasil! Relembre os grandes sucessos da banda.",
+        "type": "playlist"
+    },
+    {
+        "title": "Indo e Voltando",
+        "type": "podcast"
+    },
+    {
+        "title": "QuebraDev",
+        "type": "podcast"
+    },
+    {
+        "title": "Frozen 2 (Original Motion Picture Soundtrack)",
+        "type": "álbum"
+    },
+    {
+        "title": "Indigo Borboleta Anil",
+        "type": "álbum"
+    },
+    {
+        "title": "Sextou Gostosin",
+        "type": "playlist"
+    }
+]
+
+const podcasts = [
+    {
+        "title": "Indo e Voltando",
+        "type": "podcast"
+    },
+    {
+        "title": "QuebraDev",
+        "type": "podcast"
+    },
+    {
+        "title": "Café da Manhã",
+        "subtitle": "Spotify Studios",
+        "type": "podcast"
+    },
+    {
+        "title": "Histórias de Ninar para Garotas Rebeldes",
+        "type": "podcast"
+    },
+    {
+        "title": "Layers Ponto Tech",
+        "type": "podcast"
+    }
+]
+
+const dailymixes =  [
+    {
+        "title": "Daily Mix 1",
+        "subtitle": "Luedji Luna, Charlie Brown Jr., Tom Zé e mais",
+        "type": "daily mix"
+    },
+    {
+        "title": "Daily Mix 2",
+        "subtitle": "Epic Nature, Skyyy, HD Rain and Water e mais",
+        "type": "daily mix"
+    },
+    {
+        "title": "Daily Mix 3",
+        "subtitle": "Liniker, Linn da Quebrada, Valuá e mais",
+        "type": "daily mix"
+    },
+    {
+        "title": "Daily Mix 4",
+        "subtitle": "Cardi B, Lizzo, Doja Cat e mais",
+        "type": "daily mix"
+    },
+    {
+        "title": "Daily Mix 5",
+        "subtitle":
+            "Kristen Anderson-Lopez, Kristen Bell, Christopher Jackson e mais",
+        "type": "daily mix"
+    },
+    {
+        "title": "Daily Mix 6",
+        "subtitle": "Alaska Thunderfuck, Jessie Ware, Billie Eilish e mais",
+        "type": "daily mix"
+    }
+]
+
 //API ROUTES
 app.use('/routes',function(req, res, next){
     var routes = []
@@ -77,6 +164,19 @@ app.get('/api/playlists/:id', (re, res) => {
 
 app.get('/api/playlists', (re, res) => {
     return res.status(200).json(playlists);   
+});
+
+// PODCASTS
+app.get('/api/podcasts', (re, res) => {
+    return res.status(200).json(podcasts);   
+});
+
+// MUSICS
+app.get('/api/recently_played', (re, res) => {
+    return res.status(200).json(recentlyplayed);   
+});
+app.get('/api/dailymixes', (re, res) => {
+    return res.status(200).json(dailymixes);   
 });
 
 app.listen(21262, () => {
